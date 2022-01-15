1af43777e067b9f88123a4edb899fda621128ddd
Wagner Xu <wagnerhsu@hotmail.com>
2022-01-15 15:36:27 +0800

    Finish basic vue app

--- MyCode/01/src/App.vue
+++ MyCode/01/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <h1>{{ heading }}</h1>
+  <input v-model="text" type="text" />
+  <p>{{ text }}</p>
+  <button @click="setText">SetText</button>
   <table>
     <thead>
       <tr>
         <td>FirstName</td>
         <td>LastName</td>
         <td>Email</td>
+        <td>Photo</td>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="employee in employees" :key="employee.firstName">
+      <tr v-for="(employee, index) in employees" :key="index">
         <td>{{ employee.firstName }}</td>
         <td>{{ employee.lastName }}</td>
         <td>{{ employee.email }}</td>
+        <td><img :src="employee.photoUrl" /></td>
       </tr>
     </tbody>
   </table>
@@ -24,8 +29,18 @@ export default {
   components: {},
   data() {
     return {
+      sortBy: "firstName",
+      text: "",
       heading: "Staff Directory",
       employees: [
+         {
+          firstName: "cobbie",
+          lastName: "nurphy",
+          photoUrl: "https://randomuser.me/api/portraits/thumb/women/79.jpg",
+          email: "bobbie.murphy@example.com",
+          phone: "(925)-667-7604",
+          department: "Management",
+        },
         {
           firstName: "amelia",
           lastName: "austin",
@@ -44,6 +59,21 @@ export default {
         },
       ],
     };
+  },
+  computed: {
+    sortedEmployees() {
+      return this.employees.sort((a, b) =>
+        a[this.sortBy].localeCompare(b[this.sortBy])
+      );
+    },
+  },
+  methods: {
+    setText(event) {
+      this.text = "SetText";
+      console.log(event);
+      this.sortBy = "lastName";
+      this.employees = this.sortedEmployees;
+    },
   },
 };
 </script>
